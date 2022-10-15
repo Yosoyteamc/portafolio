1fab057f7bba6dc0db9a60b3ae3671269e52d6d9
JUAN DANIEL MOSQUERA <juandaniel@unicauca.edu.co>
2022-10-15 08:38:03 -0500

    Header create

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const typed = new Typed('.typed',{
+    stringsElement: '#text-typed',
+    smartBackspace: true,
+    typeSpeed: 60,
+    starDelay: 2000,
+    shuffle: false,
+    backDelay: 1500,
+    loop: false,
+    contentType: 'null',
+});
